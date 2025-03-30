bd3f9a3320c3f31e3261c97d24c0574c44303a69
ALkhaqani-Ahmed <ahmeducdc@gmail.com>
2025-03-30 14:54:09 +0300

    new update

--- apps/api/validateId.ts
+++
@@ -1,7 +0,0 @@
-import config from './config'
-
-const validateId = (id: string): Boolean => {
-	return id.length === config.idLength && id.split('').every(char => config.idChars.includes(char));
-};
-
-export default validateId;
